005c1e5b918dc99c43a29ddc199d6334295e3e54
samalkralik <samalkralik@gmail.com>
2025-12-16 17:46:22 +0100

    41-image-resizer

---
+++ 41-image-resizer/image_resizer.py
@@ -0,0 +1,97 @@
+# image_resizer.py
+
+from PIL import Image  # python -m pip install pillow
+
+
+class ImageResizer:
+    def __init__(self, path):
+        self.path = path
+        self.image = Image.open(path)
+
+    def resize_to(self, size: int):
+        thumb = self.image.copy()
+        thumb.thumbnail((size, size))
+        path = self.get_path(size)
+        thumb.save(path)
+
+    def get_path(self, size: int):
+        index = self.path.rindex(".")
+        extra_name = "-" + str(size)
+        # \pexels-stefanstefancik-91227.jpg
+        # \pexels-stefanstefancik-91227-400.jpg
+        # \pexels-stefanstefancik-91227-500.jpg
+        # \pexels-stefanstefancik-91227-800.jpg
+        return self.path[:index] + extra_name + self.path[index:]
+
+
+image_paths = [
+    r"C:\AKADEMIE\field.jpg",
+    r"C:\AKADEMIE\guy.jpg",
+]
+"""
+for image_path in image_paths:
+    my_image_resizer = ImageResizer(image_path)
+    # print(my_image_resizer.image)
+
+    for size in range(100, 1000, 100):
+        my_image_resizer.resize_to(size)
+
+    # my_image_resizer.resize_to(500)
+    # my_image_resizer.resize_to(800)
+
+"""
+
+
+class ImageResizer2:
+    def resize(self, path, *sizes: int):
+        """resize into multiple sizes"""
+        self.path = path
+        self.image = Image.open(path)
+
+        print(self.image)
+
+        for size in sizes:
+            self.resize_to(size)
+
+    def get_path(self, size: int):
+        index = self.path.rindex(".")
+        extra_name = "-" + str(size)
+
+        return self.path[:index] + extra_name + self.path[index:]
+
+    def resize_to(self, size: int):
+        thumb = self.image.copy()
+        thumb.thumbnail((size, size))
+
+        new_path = self.get_path(size)
+        thumb.save(new_path)
+
+        print(new_path)
+
+
+# takto to chci používat
+image_resizer = ImageResizer2()
+image_resizer.resize(r"C:\AKADEMIE\field.jpg", 200, 400, 600)
+image_resizer.resize(r"C:\AKADEMIE\guy.jpg", 400, 600, 800)
+
+
+class CharCounter:
+    """třída umožní vypočítat počet znaků v txt souboru"""
+
+
+# takto to chci používat
+# counter = CharCounter("path.txt")
+# counter.print_info()  # 7373 - path.txt
+
+"""
+2 způsoby
+
+1. každý obrázek má vlastní ImageResizer
+
+2. ImageResizer dokáže měnit velikost pro různé obrázky
+
+3. WordCounter
+
+
+
+"""
